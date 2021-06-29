200d188cc81964f9ec285783f21413958b687652
Naveen2706 <85094813+Naveen2706@users.noreply.github.com>
2021-06-29 17:45:04 +0530

    Update EmpWageComputation.java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,15 +3,14 @@ public class EmpWageComputation{
 	public static void main(String[] args){
 		
 		int isFullTime = 1;
-		int WagePerHr = 20;
-		int FullDayHr = 8;
-		
+		static final int FULLDAYHR = 8;
+		static final int WAGEPERHR = 20;
 		double empCheck = Math.floor(Math.random()*10)%2;
 
 		if(empCheck == isFullTime)
 		{
 			System.out.println("Employee is Present");
-			int DailyEmpWage = WagePerHr * FullDayHr;
+			int DailyEmpWage = WAGEPERHR * FULLDAYHR;
 			System.out.println("DailyEmpWage::" + DailyEmpWage);
 		}
 		else
